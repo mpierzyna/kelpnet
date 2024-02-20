d808a89fe2350c4a03a489920bec682df54abc76
Maximilian Pierzyna <mail@maximilian-pierzyna.de>
2024-02-20 09:14:16 -0500

    introduceensemble weighing

--- eval_ens_2staged.py
+++ eval_ens_2staged.py
@@ -15,6 +15,28 @@ from eval_ens import EnsemblePredictor
 import torchmetrics
 
 
+def get_val_score(ckpt):
+    """Get validation score from checkpoint."""
+    cb = ckpt["callbacks"]
+    cb_key = None
+    for k in cb.keys():
+        if "ModelCheckpoint" in k:
+            cb_key = k
+            break
+    return float(cb[cb_key]["best_model_score"].cpu())
+
+
+def get_ens_weights(ckpt_dir: pathlib.Path):
+    """Get ensemble weights from validation scores."""
+    seg_val_scores = np.array([
+        get_val_score(torch.load(ckpt_path))
+        for ckpt_path in sorted(ckpt_dir.glob("*.ckpt"))
+    ])
+    w = seg_val_scores / np.median(seg_val_scores)
+    w = w / w.sum()
+    return w
+
+
 def get_sea_mask_aa(ds: data.KelpTiledDataset) -> np.ndarray:
     """Get sea mask from dataset."""
     print("Getting sea mask...")
@@ -23,12 +45,19 @@ def get_sea_mask_aa(ds: data.KelpTiledDataset) -> np.ndarray:
     return is_sea.to_numpy().astype(np.uint8)
 
 
-def agg_bb_ens_pred(y_hat_bb: torch.Tensor, ts: data.RegularTileSampler) -> np.ndarray:
+def agg_bb_ens_pred(y_hat_bb: torch.Tensor, ts: data.RegularTileSampler, w: np.ndarray) -> np.ndarray:
+    """Aggregate tile-based ensemble predictions to original size (weighted majority vote)."""
     print("pred:", y_hat_bb.shape, y_hat_bb.dtype)
+
+    # Automatic dim expansion (4D for seg, 3D for clf)
+    for _ in range(y_hat_bb.ndim - w.ndim):
+        w = np.expand_dims(w, -1)
+    print("weights:", w.shape, w.dtype)
 
     # Majority voting
     y_hat_bb = (y_hat_bb > 0.5).float()  # per member (m, n_bb, 1) or (m, n_bb, b, b)
-    y_hat_bb = (y_hat_bb.mean(axis=0) > 0.5).float()  # aggregate (n_bb, 1) or (n_bb, b, b)
+    y_hat_bb = (y_hat_bb * w).sum(0)  # weighted majority vote (n_bb, 1) or (n_bb, b, b)
+    y_hat_bb = (y_hat_bb > 0.5).float()
     print("pred agg:", y_hat_bb.shape, y_hat_bb.dtype)
 
     # Reassamble tiles to original size
@@ -69,10 +98,14 @@ def get_kelp_clf_mask_aa(clf_ens_dir: pathlib.Path, ts: data.RegularTileSampler)
     y_hat_bb: torch.Tensor
     _, y_hat_bb, _ = joblib.load(clf_ens_dir / "pred_clf.joblib")
 
+    # Load val scores for weighting
+    w = get_ens_weights(clf_ens_dir)
+    print("ensemble weights:", w)
+
     # Aggregate ensembe and tiles
-    y_hat_aa = agg_bb_ens_pred(y_hat_bb, ts)
+    y_hat_aa = agg_bb_ens_pred(y_hat_bb, ts, w)
     y_hat_aa = np.ceil(y_hat_aa)  # If in doubt, consider as kelp
-    print("pred kelp fraction", y_hat_aa.sum(-1).sum(-1) / ts.orig_size_ ** 2)
+    print("pred kelp fraction:", y_hat_aa.sum(-1).sum(-1) / ts.orig_size_ ** 2)
 
     return y_hat_aa
 
@@ -86,12 +119,16 @@ def get_kelp_seg_mask_aa(seg_ens_dir: pathlib.Path, ts: data.RandomTileSampler):
     y_hat_bb: torch.Tensor
     _, y_hat_bb, _ = joblib.load(seg_ens_dir / "pred_seg.joblib")
 
+    # Load val scores for weighting
+    w = get_ens_weights(seg_ens_dir)
+    print("ensemble weights:", w)
+
     # Aggregate ensembe and tiles
-    y_hat_aa = agg_bb_ens_pred(y_hat_bb, ts)
+    y_hat_aa = agg_bb_ens_pred(y_hat_bb, ts, w)
     y_hat_aa = (y_hat_aa > 0.5).astype(np.float32)
     print("pred kelp fraction", y_hat_aa.sum(-1).sum(-1) / ts.orig_size_ ** 2)
 
-    return agg_bb_ens_pred(y_hat_bb, ts)
+    return y_hat_aa
 
 
 def get_2staged_kelp_mask_aa(clf_ens_dir: pathlib.Path, seg_ens_dir: pathlib.Path, ds: data.KelpTiledDataset) -> np.ndarray:
@@ -109,24 +146,30 @@ def get_2staged_kelp_mask_aa(clf_ens_dir: pathlib.Path, seg_ens_dir: pathlib.Pat
     return y_hat_seg_aa
 
 
-def main():
-    # ds = shared.get_submission_dataset()
-
+def main(mode: str):
     clf_ens_dir = pathlib.Path("ens_clf/20240216_041023")
     seg_ens_dir = pathlib.Path("ens_seg/20240219_163535")
-    _, _, ds = shared.get_dataset(use_channels=None, split_seed=shared.GLOBAL_SEED, tile_seed=1337, mode="seg")
 
+    if mode == "test":
+        _, _, ds = shared.get_dataset(use_channels=None, split_seed=shared.GLOBAL_SEED, tile_seed=1337, mode="seg")
+    elif mode == "submission":
+        ds = shared.get_submission_dataset()
+
+    # Get 2-staged kelp mask
     y_hat_aa = get_2staged_kelp_mask_aa(clf_ens_dir, seg_ens_dir, ds)
-    y_true_aa = ds.masks.to_numpy()
-    score = torchmetrics.functional.dice(
-        preds=torch.from_numpy(y_hat_aa),
-        target=torch.from_numpy(y_true_aa),
-    )
-    print("Dice score:", score)
+
+    if mode == "test":
+        # No true data for submission mode
+        y_true_aa = ds.masks.to_numpy()
+        score = torchmetrics.functional.dice(
+            preds=torch.from_numpy(y_hat_aa),
+            target=torch.from_numpy(y_true_aa),
+        )
+        print("Dice score:", score)
 
     print("Saving prediction...")
-    joblib.dump(y_hat_aa, "pred_2staged.joblib")
+    joblib.dump(y_hat_aa, f"pred_2staged_{mode}.joblib")
 
 
 if __name__ == "__main__":
-    main()
+    main("test")
